b4d233566f1d5d5484f37c26611d0afab99d43da
Jordi Otero <jordiotero15@gmail.com>
2024-09-23 20:23:10 -0300

    Users

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import userController from "./src/controllers/users.controllers.js";
+
+const server = express();
+
+const port = 8080;
+
+const ready = () => console.log("Server ready on port" + port);
+
+server.listen(port, ready);
+
+server.use(express.urlencoded({ extended: true }));
+
+server.use(express.json());
+
+server.get("/api/users", userController.readUsers);
+server.post("/api/users", userController.createUser);
---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,50 @@
+import usersManager from "../data/managers/users.fs.js";
+
+class UserControllers {
+  constructor() {}
+
+  async readUsers(req, res) {
+    try {
+      const { role } = req.query;
+      const data = await usersManager.readAll(role);
+      if (data.length > 0) {
+        return res.status(200).json({ data, message: "users fetched" });
+      } else {
+        //   return res.status(404).json({ message: "Users not found" });
+        const error = new Error("Users not found");
+        error.statusCode = 404;
+        throw error;
+      }
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(error.statusCode || 500)
+        .json({ message: error.message || "API Error" });
+    }
+  }
+
+  async createUser(req, res) {
+    try {
+      const data = req.body;
+      const { email, password } = data;
+      if (!email || !password) {
+        const error = new Error("Email and password are required");
+        error.statusCode = 400;
+        throw error;
+      }
+      const userId = await usersManager.create(data);
+      return res
+        .status(201)
+        .json({ message: `User created with id ${userId}` });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(error.statusCode || 500)
+        .json({ message: error.message || "API Error" });
+    }
+  }
+}
+
+const userController = new UserControllers();
+
+export default userController;
