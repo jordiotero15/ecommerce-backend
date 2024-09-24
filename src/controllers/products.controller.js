aacf234cae6b803908aa1feb6173d777165e1276
Jordi Otero <jordiotero15@gmail.com>
2024-09-24 15:15:34 -0300

    Fixing

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,7 +1,32 @@
 //import productsManager from "../data/products.manager.js"
 
-async function getAllProducts(req, res) {}
-async function getProduct(req, res) {}
+async function getAllProducts(req, res, next) {
+  try {
+    let { category } = req.query;
+    let response;
+    if (!category) {
+      response = await productsManager.readAll();
+    } else {
+      response = await productsManager.readAll(category);
+    }
+    if (response.length > 0) {
+      return res.status(200).json({ message: "Products read", response });
+    } else {
+      const error = new Error("Products not founds");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    return next(error);
+  }
+}
+async function getProduct(req, res) {
+  try {
+    const {pid} = req.params;
+  } catch (error) {
+    
+  }
+}
 async function createGet(req, res) {}
 async function createProduct(req, res) {}
 async function updateProduct(req, res) {
